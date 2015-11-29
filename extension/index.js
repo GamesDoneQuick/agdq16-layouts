022364918bf56f566bd4b305caefbc5515a456b3
Alex Van Camp <email@alexvan.camp>
2015-11-28 22:38:06 -0600

    Activate bids extension.

--- extension/index.js
+++ extension/index.js
@@ -15,12 +15,12 @@ module.exports = function(nodecg) {
         process.exit(1);
     }*/
 
-    /*try {
+    try {
         require('./bids')(nodecg);
     } catch (e) {
         nodecg.log.error('Failed to load "bids" lib:', e.stack);
         process.exit(1);
-    }*/
+    }
 
     /*try {
         require('./total')(nodecg);
