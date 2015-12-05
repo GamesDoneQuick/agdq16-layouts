392a8708d7b3bbf211db1ec45fed05b9f016f11b
Alex Van Camp <email@alexvan.camp>
2015-12-04 20:53:56 -0600

    [stopwatches] Format hours to one digit instead of two

--- extension/stopwatches.js
+++ extension/stopwatches.js
@@ -265,7 +265,7 @@ function msToTime(duration) {
         minutes = parseInt((duration/(1000*60))%60),
         hours = parseInt((duration/(1000*60*60))%24);
 
-    hours = (hours < 10) ? '0' + hours : hours;
+    hours = (hours < 10) ? hours : hours;
     minutes = (minutes < 10) ? '0' + minutes : minutes;
     seconds = (seconds < 10) ? '0' + seconds : seconds;
 
