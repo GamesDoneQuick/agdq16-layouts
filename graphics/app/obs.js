843078889c854d30b32c61eab7500b845e4be90c
Alex Van Camp <email@alexvan.camp>
2015-12-06 09:39:20 -0600

    [obs] Retry connection every 5 seconds if connection failed or lost

--- graphics/app/obs.js
+++ graphics/app/obs.js
@@ -1,10 +1,23 @@
 /* global define, OBSRemote */
 define([
-    'layout'
-], function (layout) {
+    'layout',
+    'debounce'
+], function (layout, debounce) {
     'use strict';
 
     var obs = new OBSRemote();
+
+    var retryConnection = debounce(connect, 5000);
+
+    var _handleSceneSwitch = debounce(function () {
+        obs.getCurrentScene(function (scene) {
+            scene.sources.some(function(source) {
+                if (source.name.indexOf('Layout ') === 0) {
+                    layout.changeTo(source.name.split(' ')[1]);
+                }
+            });
+        });
+    }, 10);
 
     obs.onConnectionOpened = function () {
         console.log('OBS | Connected.');
@@ -13,37 +26,34 @@ define([
 
     obs.onConnectionClosed = function () {
         console.log('OBS | Connection closed.');
+        retryConnection();
     };
 
     obs.onConnectionFailed = function () {
         console.log('OBS | Failed to connect.');
+        retryConnection();
     };
 
     obs.onAuthenticationFailed = function (remainingAttempts) {
         console.log('OBS | Authentication failed, %s attempts remaining.', remainingAttempts);
     };
 
-    obs.onSceneSwitched = _handleSceneSwitch;
+    obs.onSceneSwitched = function(scene) {
+        console.log('OBS | Switched to scene "%s".', scene.name);
+        _handleSceneSwitch();
+    };
     obs.onSourceChanged = _handleSceneSwitch;
+    obs.onSourceAddedOrRemoved = _handleSceneSwitch;
 
-    function _handleSceneSwitch() {
-        obs.getCurrentScene(function (scene) {
-            console.log('OBS | Switched to scene "%s".', scene.name);
-
-            scene.sources.some(function(source) {
-                if (source.name.indexOf('Layout ') === 0) {
-                    layout.changeTo(source.name.split(' ')[1]);
-                }
-            });
-        });
+    function connect() {
+        if (nodecg.bundleConfig && nodecg.bundleConfig.obs) {
+            obs.connect(nodecg.bundleConfig.obs.host, nodecg.bundleConfig.obs.password);
+        } else {
+            obs.connect();
+        }
     }
 
-    if (nodecg.bundleConfig && nodecg.bundleConfig.obs) {
-        obs.connect(nodecg.bundleConfig.obs.host, nodecg.bundleConfig.obs.password);
-    } else {
-        obs.connect();
-    }
-
+    connect();
 
     return obs;
 });
