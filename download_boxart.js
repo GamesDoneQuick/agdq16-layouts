9b7e3005e7ce0cd3e0e130f46a25fc2b904f8d05
Alex Van Camp <email@alexvan.camp>
2016-01-15 14:21:43 -0600

    Refactor download_boxart script

--- download_boxart.js
+++ download_boxart.js
@@ -1,36 +1,49 @@
+/* jshint -W106 */
 'use strict';
 
 var fs = require('fs');
+var rp = require('request-promise');
 var request = require('request');
-var schedule = JSON.parse(fs.readFileSync('schedule.json'));
 var failed = [];
 
-fetchBoxart(schedule[0]);
+var schedule;
+rp({
+    uri: 'https://gamesdonequick.com/tracker/search',
+    qs: {
+        type: 'run',
+        event: 17
+    },
+    json: true
+}).then(function(s) {
+    schedule = s;
+    fetchBoxart(schedule[0]);
+});
 
 function fetchBoxart(run) {
-    console.log('Fetching %s...', run.boxart.url);
+    var boxartUrl = 'http://static-cdn.jtvnw.net/ttv-boxart/'+run.fields.name+'-469x655.jpg';
+    console.log('Fetching %s...', boxartUrl);
 
-    var filename = new Buffer(run.name).toString('base64');
+    var filename = new Buffer(run.fields.display_name).toString('base64');
     var filepath = 'graphics/img/boxart/' + filename + '.jpg';
 
-    var stream = request(run.boxart.url);
+    var stream = request(boxartUrl);
     stream.pipe(fs.createWriteStream(filepath));
 
     stream.on('error', function(err) {
         failed.push(run);
-        console.error('Failed to fetch', run.boxart.url);
+        console.error('Failed to fetch', boxartUrl);
         fetchNext(run);
     });
 
     stream.on('end', function() {
-        console.log('Successfully fetched', run.boxart.url);
+        console.log('Successfully fetched', boxartUrl);
         fetchNext(run);
     });
 }
 
 function fetchNext(run) {
-    if (run.order < schedule.length - 2) {
-        fetchBoxart(schedule[run.order]);
+    if (run.fields.order < schedule.length - 2) {
+        fetchBoxart(schedule[run.fields.order]);
     } else {
         if (failed.length > 0) {
             console.warn('%s downloads failed, writing to failed.json', failed.length);
